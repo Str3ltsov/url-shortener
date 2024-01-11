721d71a4c9652ef8dd816400a82da856307d6069
Str3ltsov <andtaress@live.com>
2024-01-11 21:57:17 +0200

    Url shortener home page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UrlShortenerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return inertia()->render('Index');
-});
+Route::get('/', [UrlShortenerController::class, 'index'])->name('home');
