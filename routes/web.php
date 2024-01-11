733067aee38e2589115e085e78b0e760aa2fcfa2
Str3ltsov <andtaress@live.com>
2024-01-11 23:20:38 +0200

    Routes for store and show

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [UrlShortenerController::class, 'index'])->name('home');
+Route::get('/', [UrlShortenerController::class, 'index'])->name('urlShortener');
+Route::post('/', [UrlShortenerController::class, 'store'])->name('generateShortUrl');
+Route::get('/{folder?}/{hash}', [UrlShortenerController::class, 'show'])->name('redirectToUrl');
