4d48dc74bf4654031c75c74ebe6f4b1340786beb
Str3ltsov <andtaress@live.com>
2024-01-11 23:02:46 +0200

    Short url generation

--- app/Http/Controllers/UrlShortenerController.php
+++ app/Http/Controllers/UrlShortenerController.php
@@ -2,13 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateShortUrlRequest;
+use App\Services\UrlShortenerService;
 use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+use Exception;
 
 class UrlShortenerController extends Controller
 {
+    public function __construct(private UrlShortenerService $service)
+    {
+    }
+
     public function index(): Response
     {
         return inertia()->render('Home');
+    }
+
+    public function store(CreateShortUrlRequest $request): RedirectResponse
+    {
+        $validInputs = $request->validated();
+
+        try {
+            $existingShortUrl = $this->service->getShortUrlWithUsedUrl($validInputs['url']);
+
+            if (!$existingShortUrl) {
+                $hash = $this->service->generateRandomHash();
+                $this->service->createShortUrl($hash, $validInputs);
+                $existingShortUrl = $this->service->getShortUrlWithUsedUrl($validInputs['url']);
+            }
+
+            return back()
+                ->with([
+                    'success' => 'Successfully created short url.',
+                    'shortUrl' => $this->service->generateShortUrl($existingShortUrl)
+                ]);
+        } catch (Exception $exception) {
+            return back()
+                ->with('error', env('APP_ENV') === 'local' ? $exception : $exception->getMessage());
+        }
     }
 }
---
+++ app/Services/UrlShortenerService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\ShortUrl;
+
+class UrlShortenerService
+{
+    // Generates random alphanumeric hash.
+    public final function generateRandomHash(int $length = 6): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomHash = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomHash .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomHash;
+    }
+
+    // Finds and retrieves short url record where entered url is already being used.
+    public final function getShortUrlWithUsedUrl(string $url): object
+    {
+        return ShortUrl::where('url', $url)->first();
+    }
+
+    // Creates new short url record.
+    public final function createShortUrl(string $hash, array $validInputs): void
+    {
+        ShortUrl::create([
+            'hash' => $hash,
+            'url' => $validInputs['url'],
+            'folder' => $validInputs['folder'] ?? null
+        ]);
+    }
+
+    // Generates short url.
+    public final function generateShortUrl(object $shortUrl): string
+    {
+        return env('APP_URL') . ':' . env('APP_PORT') . '/' . $shortUrl->folder . '/' . $shortUrl->hash;
+    }
+}
